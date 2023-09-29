2462329b79af6b4e901bca15f80f55bcbf937486
TonyPeMo <tonypm2002@gmail.com>
2023-09-29 15:01:42 +0200

    creacion clase dispositiu y correcion clase tablet

---
+++ MobileWorldCongress/Tablet.java
@@ -0,0 +1,36 @@
+package MobileWorldCongress;
+
+public class Tablet extends Dispositiu {
+
+    private double nPolsades;
+
+    public Tablet(String marca, String model, double preuBase, double nPolsades) {
+        super(marca, model, preuBase);
+        this.nPolsades = nPolsades;
+    }
+
+    public double getnPolsades() {
+        return nPolsades;
+    }
+
+    public void setnPolsades(double nPolsades) {
+        this.nPolsades = nPolsades;
+    }
+
+    // Altres metodes
+    @Override
+    public String toString() {
+        return "Tablet{" + "nPolsades=" + nPolsades + '"' + " " + super.toString() + '}';
+    }
+
+    @Override
+    public double preuFinal(){
+        double preuFinal;
+        double preuBase;
+        
+        preuBase = getPreuBase();
+        preuFinal = preuBase * 2;
+        return preuFinal;
+    }
+
+}
